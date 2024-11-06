6e40ec67022f7fcfa45808491814cae75aebba9e
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-11-06 17:04:20 -0300

    feat: Implementa metadados dinâmicos para páginas de serviços

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,49 @@
 import { Container } from "@/components/Container"
 import { Hero } from "@/components/Hero"
-import { getItemBySlug } from "@/utils/action/get-data"
+import { getItemBySlug, PostProps } from "@/utils/action/get-data"
 import { Phone } from "lucide-react"
+import { Metadata } from "next"
 import Image from "next/image"
+
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+    const { slug } = await params;
+
+    try {
+        const { objects } = await getItemBySlug(slug)
+
+        if (!objects) {
+            return {
+                title: "DevMotors - Sua oficina especializada",
+                description: "Oficina de carros em Florianópolis",
+            }
+        }
+
+        return {
+            title: `DevMotors - ${objects[0].title}`,
+            description: objects[0].metadata.description.text,
+            openGraph: {
+                title: `DevMotors - ${objects[0].title}`,
+                images: [objects[0].metadata.banner.url]
+            },
+            robots: {
+                index: true,
+                follow: true,
+                nocache: true,
+                googleBot: {
+                    index: true,
+                    follow: true,
+                    noimageindex: true,
+                }
+            }
+        }
+
+    } catch (err) { //eslint-disable-line  
+        return {
+            title: "DevMotors - Sua oficina especializada",
+            description: "Oficina de carros em Florianópolis",
+        }
+    }
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const { slug } = await params
